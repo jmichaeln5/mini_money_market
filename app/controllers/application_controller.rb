707dcbf87b5a8e1fc8bc452c4947200d19e95c0a
Jon Norton <jmichaeln5@gmail.com>
2018-08-09 18:28:36 -0400

    Login redirect to dash + icon of lender + borrower on respective dashboards

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(resource)
+    # current_user_path
+    if current_lender
+      pages_lender_dash_path
+    elsif current_borrower
+      pages_borrower_dash_path
+    end
+  end
+
 end
